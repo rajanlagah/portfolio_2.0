c681df013cbdf3bcaa67ab312ca9049dadcf4425
Rajan lagah <rajanlagah@gmail.com>
2024-01-08 00:04:53 +0530

    Fixed exception

--- src/component/canvas/Earth.jsx
+++ src/component/canvas/Earth.jsx
@@ -1,44 +1,65 @@
-import React, { Suspense } from "react";
+import React, { Component, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+class Earth extends Component {
+  render() {
+    const earth = useGLTF("./planet/scene.gltf");
+    return (
+      <primitive
+        object={earth.scene}
+        scale={2.5}
+        position-y={0}
+        rotation-y={0}
+      />
+    );
+  }
+}
 
-  return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
-  );
-};
+class EarthCanvas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-const EarthCanvas = () => {
-  return (
-    <Canvas
-      shadows
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 10],
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
+  componentDidCatch(error, info) {
+    console.error("Error caught by componentDidCatch:", error, info);
+    this.setState({ hasError: true });
+  }
 
-        <Preload all />
-      </Suspense>
-    </Canvas>
-  );
-};
+  render() {
+    if (this.state.hasError) {
+      return <div>.</div>;
+    }
+
+    return (
+      <Canvas
+        shadows
+        frameloop="demand"
+        dpr={[1, 2]}
+        gl={{ preserveDrawingBuffer: true }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 10]
+        }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            autoRotate
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Earth />
+          <Preload all />
+        </Suspense>
+      </Canvas>
+    );
+  }
+}
 
 export default EarthCanvas;
